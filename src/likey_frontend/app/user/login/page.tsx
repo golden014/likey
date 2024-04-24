68dab0e6268692f84ee4258b8eed2a18aabdeee2
Vito <98193285+vitountariady@users.noreply.github.com>
2024-04-24 09:55:44 +0700

    authenticate internet identity on login page

--- src/likey_frontend/app/user/login/page.tsx
+++ src/likey_frontend/app/user/login/page.tsx
@@ -4,12 +4,12 @@ import { log } from "console"
 import { ChangeEvent, useState } from "react"
 import { likey_backend, createActor } from '../../../../declarations/likey_backend';
 import { UserProfilePayload } from "../../../../declarations/likey_backend/likey_backend.did";
-
-async function getStaticProps() {
-    const idxd = process.env.CANISTER_ID_LIKEY_BACKEND            
-}
+import { AuthClient } from "@dfinity/auth-client";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () =>{
+    const router = useRouter()
+
     const [loginData, setLoginData] = useState({
         email : "",
         password : ""
@@ -21,23 +21,37 @@ const LoginPage = () =>{
         }))
     }
 
+    // One day in nanoseconds
+    const days = BigInt(1);
+    const hours = BigInt(24);
+    const nanoseconds = BigInt(3600000000000);
+
+    const defaultOptions = {
+        createOptions: {
+          idleOptions: {
+            // Set to true if you do not want idle functionality
+            disableIdle: true,
+          },
+        },
+        loginOptions: {
+          identityProvider: "https://identity.ic0.app",
+          maxTimeToLive: days * hours * nanoseconds,
+        },
+      };
+
     const handleLogin = async (e:any) =>{
-        e.preventDefault()
+        e.preventDefault();
+        const authClient = await AuthClient.create(defaultOptions.createOptions);
 
-        let a: UserProfilePayload = {
-            'height' : 5,
-            'profile_picture_link' : "test",
-            'education' : 3,
-            'description' : "Test Update",
-            'first_name' : "Joshua",
-            'last_name' : "Caris",
-            'religion' : "Atheist",
+        if (await authClient.isAuthenticated()) {
+            router.push('/home')
+        }else{
+            authClient.login(defaultOptions.loginOptions).then(()=>{
+                router.push('/home')
+            }).catch((e)=>{
+                console.log(e)
+            })
         }
-
-        console.log(a);
-        
-        let b = await likey_backend.update_user(BigInt(1),a);
-        console.log(b)
     }
 
     return(
@@ -45,14 +59,14 @@ const LoginPage = () =>{
             <form onSubmit={handleLogin} className='w-1/2 h-full flex flex-col items-center justify-center'>
                 <p className="text-text text-title font-bold">Welcome Back!</p>
                 <br />
-                <div className='w-4/5 h-auth_form_input'>
+                {/* <div className='w-4/5 h-auth_form_input'>
                     <input onChange={handleChange} type="email" placeholder='Email' name='email' className='text-text w-full h-full p-6 rounded-default border border-border_placeholder bg-background shadow-xl' ></input>
                 </div>
                 <div className='w-4/5 h-auth_form_input my-6'>
                     <input onChange={handleChange} type="password" placeholder='Password' name='password' className='text-text w-full h-full p-6 rounded-default border border-border_placeholder bg-background shadow-xl'></input>
-                </div>
+                </div> */}
                 <div className='w-4/5 h-auth_form_input'>
-                    <button className='w-full h-full bg-main rounded-default shadow-xl'>Login</button>
+                    <button className='text-white w-full h-full bg-main rounded-default shadow-xl'>Login using Internet Identity</button>
                 </div>
                 <div className='mt-6 text-text'>
                     Don{"'"}t have an account?
