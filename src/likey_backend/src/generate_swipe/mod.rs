7a841932c087db0c2e0ae0259e2fd96b22783be0
Josua Golden <josuagolden14@gmail.com>
2024-05-05 21:32:48 +0700

    get current time and date
    manually get current time and date because error when using chrono or time

--- src/likey_backend/src/generate_swipe/mod.rs
+++ src/likey_backend/src/generate_swipe/mod.rs
@@ -3,7 +3,7 @@
 //the vector must be filtered based on age, religion, hobbies
 //additional constraint: the vector must only contain new people (the one that never been swapped before) -> check from interest
 
-use crate::{Error, User, _get_user};
+use crate::{Error, User, _get_user, HOBBY_STORAGE};
 
 
 
@@ -15,22 +15,30 @@ fn generate_swipe(user_id: Vec<u8>) -> Result<Option<Vec<Vec<u8>>>, Error>{
         return Result::Err(Error::NotFound { msg: "Invalid user ID".to_string() });
     }
 
-    //get user's current swipe
-    let current_user_swipe_limit = match _get_user(&user_id) {
-        Some(user) => user.current_swipe,
-        None => -1
-    };
-
-    //jka gadapat user id nya -> invaid user ID
-    if current_user_swipe_limit == -1 {
-        return Result::Err(Error::NotFound { msg: "Invalid user ID".to_string() });
-    }
+    let swipe_filters = curr_user.unwrap().swipe_filters;
 
     //next step: bikin vectors of user ids yang match dengan filter yang ada
+
+    //filters:
+    //dari object user
+    //user - gender harus berlawanan arah dengan curr user's gender
+    //user - education
+    //user - religion
+    //user - height (masuk ke range yg dimau oleh user)
+    //user - age
+    
+    //dari object hobby
+    //
 
     
     //TODO: bikin filter nya utk religion, age, dll
     return Result::Ok(None);
 
 }
+
+// fn filter_by_hobbies(hobbies: Vec<String>) -> Option<Vec<Vec<u8>>> {
+//     HOBBY_STORAGE.with(|m|) {
+        
+//     }
+// }
 
--- src/likey_backend/src/lib.rs
+++ src/likey_backend/src/lib.rs
@@ -5,21 +5,19 @@ use candid::{Decode, Encode};
 use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
 use ic_stable_structures::storable::Blob;
 use ic_stable_structures::{BoundedStorable, DefaultMemoryImpl, StableBTreeMap, Storable, StableVec};
-use chrono::prelude::*;
+use ic_cdk::api::time;
 use std::collections::HashMap;
+
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+// use std::time::{SystemTime, UNIX_EPOCH};
+// use time::{PrimitiveDateTime, Duration};
 // use serde::de::IntoDeserializer;
 // use std::ptr::null;
 use std::{borrow::Cow, cell::RefCell};
 // use regex::Regex;
+
 use std::{self, result};
-// use argon2::{
-//     password_hash::{
-//         rand_core::OsRng,
-//         PasswordHash, PasswordHasher, PasswordVerifier, SaltString
-//     },
-//     Argon2
-// };
-// use argon2::{Config};
+
 
 //module generate swipe
 pub mod generate_swipe;
@@ -43,6 +41,7 @@ struct User {
     current_swipe: i32,
     filter_access: bool,
     swipe_filters: HashMap<String, FilterAttribute>
+    //TODO : tambahin attribute last time reset current swipe
 }
 
 #[derive(candid::CandidType, Clone, Serialize, Deserialize, Default)]
@@ -298,8 +297,104 @@ fn _get_user(id: &Vec<u8>) -> Option<User> {
 //Greet
 #[ic_cdk::query]
 fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+    // // // Get the current system time
+    // let current_time = SystemTime::now();
+
+    // // Convert system time to a Duration since the UNIX epoch
+    // let duration_since_epoch = current_time.duration_since(UNIX_EPOCH).expect("Time went backwards");
+
+    // // Convert duration to seconds
+    // let seconds = duration_since_epoch.as_secs();
+
+    // // Convert seconds to a format string
+    // let date_string = format_timestamp(seconds);
+    
+    // format!("Current time in date string format: {}", date_string)
+    // let curr_date = Utc::now().naive_utc().date();
+    // curr_date.to_string()
+
+    let nanoseconds_since_epoch = time();
+
+    // Convert nanoseconds to seconds
+    let seconds_since_epoch = nanoseconds_since_epoch / 1_000_000_000;
+
+    // Create a Duration representing the time since the UNIX epoch
+    let duration = Duration::new(seconds_since_epoch as u64, 0);
+
+    // Calculate the current date and time
+    let (year, month, day, hour, minute, second) = calculate_date_time(duration);
+
+    // Format the date and time components into a string
+    let formatted_time = format!("{}-{:02}-{:02} {:02}:{:02}:{:02}", year, month, day, hour, minute, second);
+
+    format!("Formatted time: {}", formatted_time)
+
 }
+
+fn calculate_date_time(duration: Duration) -> (i64, u32, u32, u32, u32, u32) {
+    // Calculate the number of seconds since the UNIX epoch
+    let seconds = duration.as_secs();
+
+    // Calculate the number of days
+    let days = seconds / (24 * 3600);
+
+    // Calculate the number of remaining seconds after removing the days
+    let remaining_seconds = seconds % (24 * 3600);
+
+    // Calculate the number of hours
+    let hours = remaining_seconds / 3600;
+
+    // Calculate the number of remaining seconds after removing the hours
+    let remaining_seconds = remaining_seconds % 3600;
+
+    // Calculate the number of minutes
+    let minutes = remaining_seconds / 60;
+
+    // Calculate the number of remaining seconds after removing the minutes
+    let seconds = remaining_seconds % 60;
+
+    // Calculate the year, month, and day based on the number of days
+    let mut year = 1970;
+    let mut days_left = days;
+    let mut month = 1;
+    let mut day = 1;
+
+    while days_left >= 365 {
+        let is_leap_year = year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+        let days_in_year = if is_leap_year { 366 } else { 365 };
+
+        if days_left >= days_in_year {
+            year += 1;
+            days_left -= days_in_year;
+        } else {
+            break;
+        }
+    }
+    let days_in_month = [31, if year % 4 == 0 { 29 } else { 28 }, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    for (i, days_in_current_month) in days_in_month.iter().enumerate() {
+        if days_left >= *days_in_current_month as u64 {
+            month += 1;
+            days_left -= *days_in_current_month as u64;
+        } else {
+            day += days_left as u32;
+            break;
+        }
+    }
+
+    (year, month, day, hours as u32, minutes as u32, seconds as u32)
+}
+
+// fn format_timestamp(seconds: u64) -> String {
+//     // Convert seconds since UNIX epoch to a PrimitiveDateTime
+//     let datetime = PrimitiveDateTime::unix_epoch() + Duration::seconds(seconds as i64);
+
+//     // Define the format string for the desired date format
+//     let format_string = "%Y-%m-%d %H:%M:%S";
+
+//     // Format the PrimitiveDateTime as a date string
+//     datetime.format(format_string)
+// }
 
 #[ic_cdk::query]
 fn get_user(id: Vec<u8>) -> Result<User, Error> {
@@ -340,26 +435,26 @@ fn get_feeds(id: Vec<u8>) -> Result<Option<Vec<Vec<u8>>>, Error> {
 
     //check if the feeds for the user for today's date already generated or not
 
-    let curr_date = Utc::now().naive_utc().date();
-    let mut already_generated = false;
+    // let curr_date = Utc::now().naive_utc().date();
+    // let mut already_generated = false;
 
-    let p: Blob<29> = Blob::from_bytes(std::borrow::Cow::Borrowed(&id));
+    // let p: Blob<29> = Blob::from_bytes(std::borrow::Cow::Borrowed(&id));
 
-    let mut curr_swipe_pool: Option<SwipePool> = None;
+    // let mut curr_swipe_pool: Option<SwipePool> = None;
     
-    SWIPE_POOL_STORAGE.with(|service| {
-        for (_, swipe_pool) in service.borrow().iter() {
-            //belom di test -> masih nunggu ke generate dlu
-            if swipe_pool.owner_id == id && swipe_pool.date == curr_date.to_string(){
-                already_generated = true;
-                curr_swipe_pool = Some(swipe_pool);
-            }
-        }    
-    });
+    // SWIPE_POOL_STORAGE.with(|service| {
+    //     for (_, swipe_pool) in service.borrow().iter() {
+    //         //belom di test -> masih nunggu ke generate dlu
+    //         if swipe_pool.owner_id == id && swipe_pool.date == curr_date.to_string(){
+    //             already_generated = true;
+    //             curr_swipe_pool = Some(swipe_pool);
+    //         }
+    //     }    
+    // });
 
-    if already_generated == true {
-        return Result::Ok(Some(curr_swipe_pool.unwrap().user_ids))
-    }
+    // if already_generated == true {
+    //     return Result::Ok(Some(curr_swipe_pool.unwrap().user_ids))
+    // }
 
     //dummy
     return Result::Err(Error::NotFound { msg: "Invalid user ID".to_string() });
